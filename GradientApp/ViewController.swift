03890051af90e6a39c6c224294f7b01d459099cf
Artiom P <apoluyanovich@gmail.com>
2021-03-28 23:24:55 +0300

    added functional

--- GradientApp/ViewController.swift
+++ GradientApp/ViewController.swift
@@ -11,19 +11,64 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colourView: UIView!
     
-    @IBOutlet weak var readValueLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
     
-    @IBOutlet weak var readSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
+    private var viewColour = CGColor(red: 1.0,
+                                     green: 1.0,
+                                     blue: 1.0,
+                                     alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSliders()
+        changeBackgroundColour()
     }
 
-
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        changeBackgroundColour()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        changeBackgroundColour()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeBackgroundColour()
+    }
+    
+    private func setupSliders() {
+        redSlider.minimumValue = 0.0
+        redSlider.maximumValue = 1.0
+        redSlider.value = 0.25
+        redSlider.tintColor = .red
+        
+        greenSlider.minimumValue = 0.0
+        greenSlider.maximumValue = 1.0
+        greenSlider.value = 0.5
+        greenSlider.tintColor = .green
+        
+        blueSlider.minimumValue = 0.0
+        blueSlider.maximumValue = 1.0
+        blueSlider.value = 0.75
+        blueSlider.tintColor = .blue
+    }
+    
+    private func changeBackgroundColour() {
+        viewColour = CGColor(red: CGFloat(redSlider.value),
+                             green: CGFloat(greenSlider.value),
+                             blue: CGFloat(blueSlider.value),
+                             alpha: 1.0)
+        colourView.backgroundColor = UIColor(cgColor: viewColour)
+    }
 }
 
