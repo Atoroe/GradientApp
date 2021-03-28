3116eaede31f79e1ce6a109a18453c039466dd38
Artiom P <apoluyanovich@gmail.com>
2021-03-28 22:51:35 +0300

    storyboard initialised

--- GradientApp/ViewController.swift
+++ GradientApp/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colourView: UIView!
+    
+    @IBOutlet weak var readValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var readSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
