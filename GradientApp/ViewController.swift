cd6f6373f57ce7ed3ea8e03d9b3929c13ef00d2b
Artiom P <apoluyanovich@gmail.com>
2021-03-29 15:25:04 +0300

    optimised code

--- GradientApp/ViewController.swift
+++ GradientApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var colourView: UIView!
     
     @IBOutlet weak var redValueLabel: UILabel!
@@ -19,56 +19,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    private var viewColour = CGColor(red: 1.0,
-                                     green: 1.0,
-                                     blue: 1.0,
-                                     alpha: 1.0)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupSliders()
         changeBackgroundColour()
+        setupLableText()
     }
-
+    
     @IBAction func redSliderAction() {
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        setupLableText()
         changeBackgroundColour()
     }
     
     @IBAction func greenSliderAction() {
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        setupLableText()
         changeBackgroundColour()
     }
     
     @IBAction func blueSliderAction() {
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        setupLableText()
         changeBackgroundColour()
     }
     
-    private func setupSliders() {
-        redSlider.minimumValue = 0.0
-        redSlider.maximumValue = 1.0
-        redSlider.value = 0.25
-        redSlider.tintColor = .red
-        
-        greenSlider.minimumValue = 0.0
-        greenSlider.maximumValue = 1.0
-        greenSlider.value = 0.5
-        greenSlider.tintColor = .green
-        
-        blueSlider.minimumValue = 0.0
-        blueSlider.maximumValue = 1.0
-        blueSlider.value = 0.75
-        blueSlider.tintColor = .blue
+    private func changeBackgroundColour() {
+        colourView.backgroundColor = UIColor(cgColor: CGColor(red:                                                              CGFloat(redSlider.value),
+                                                              green: CGFloat(greenSlider.value),
+                                                              blue: CGFloat(blueSlider.value),
+                                                              alpha: 1.0))
     }
     
-    private func changeBackgroundColour() {
-        viewColour = CGColor(red: CGFloat(redSlider.value),
-                             green: CGFloat(greenSlider.value),
-                             blue: CGFloat(blueSlider.value),
-                             alpha: 1.0)
-        colourView.backgroundColor = UIColor(cgColor: viewColour)
+    private func setupLableText() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
     }
 }
 
